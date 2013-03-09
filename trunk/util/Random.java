3579d9d19150ac0ee463ae50a0287b0cac164a34
Ben.Powerbot@gmail.com <Ben.Powerbot@gmail.com@7edbfb4f-ca64-2465-783b-f51b566d6410>
2013-03-09 23:30:15 +0000

    Added nextGaussian() function for increased randomisation.

--- trunk/util/Random.java
+++ trunk/util/Random.java
@@ -10,4 +10,15 @@ public class Random {
 	public static double nextDouble(){
 		return random.nextDouble();
 	}
+  
+  public static int nextGaussian(final int min, final int max, final int sd) {
+     if (min == max) {
+        return min;
+     }
+     int x = min - 1;
+     while (x < min || x >= max) {
+        x = (int) (random.nextGaussian() * sd + (min + (max - min) / 2));
+     }
+     return x;
+  }
 }
