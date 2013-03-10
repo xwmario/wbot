742629b5b6d3a2a4b15bce3d4420b546267cbb24
iJavaServices <ijavaservices@gmail.com>
2013-03-10 00:59:35 +0000

    Magic Base

---
+++ methods/Magic.java
@@ -0,0 +1,20 @@
+package methods;
+
+import enums.Spell;
+
+/**
+ * Author iJava
+ */
+public class Magic {
+
+    public static boolean castSpell(Spell spell) {
+        if (bot.script.methods.Game.getTab() != bot.script.enums.Tab.MAGIC) {
+            bot.script.methods.Game.openTab(bot.script.enums.Tab.MAGIC);
+        }
+        if (spell.getComponent() != null) {
+            return spell.getComponent().interact("Cast");
+        }
+        return false;
+    }
+
+}
