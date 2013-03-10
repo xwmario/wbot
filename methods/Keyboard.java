f0364a07f53f8c589767ab124cb0bd1c22173691
roflxgod <roflxgod@gmail.com>
2013-03-10 13:32:56 +0000

    Updated Keyboard class, added TilePath for walking

--- methods/Keyboard.java
+++ methods/Keyboard.java
@@ -7,88 +7,194 @@ import nl.wbot.bot.Bot;
 
 import bot.script.util.Random;
 
-public class Keyboard extends Methods{
-	public static void typeText(String text, boolean enter){
-		sendKeys(text, enter, 100, 200);
+/**
+ *
+ * @author Roflgod
+ *
+ */
+public class Keyboard extends Methods {
+
+	public static void pressKey(final char ch, final int code, final int delay, final int mask) {
+		final KeyEvent e = getKeyEvent(KeyEvent.KEY_PRESSED, delay, mask, code, ch);
+		getKeyboard()._keyPressed(e);
+		// Action keys do not send key typed events.
+		// The character of an action key is VK_UNDEFINED ((char) 65535).
+		if (ch != KeyEvent.VK_UNDEFINED) {
+			getKeyboard()._keyTyped(e);
+		}
 	}
-	
+
+	/**
+	 * Used for pressing action keys. Eg. left, right, up, down, home
+	 *
+	 * @param code Key code from {@link KeyEvent}.
+	 * @param delay The time until the key is held down.
+	 */
+	public static void pressKey(final int code, final int delay) {
+		pressKey(KeyEvent.CHAR_UNDEFINED, code, delay, 0);
+	}
+
+	public static void pressKey(final int code) {
+		pressKey(KeyEvent.CHAR_UNDEFINED, code, 0, 0);
+	}
+
+	/**
+	 * Used for pressing "typeable" keys. Eg. 'a', 'b', 'c', '6'.
+	 *
+	 * @param ch The char to press.
+	 * @param delay The time until the key is held down.
+	 * @param mask The mask to press this key with.
+	 */
+	public static void pressKey(final char ch, final int delay, final int mask) {
+		pressKey(ch, ch, delay, mask);
+	}
+
+	public static void pressKey(final char ch, final int delay) {
+		pressKey(ch, ch, delay, 0);
+	}
+
 	public static void pressKey(final char ch) {
-		KeyEvent ke;
-		ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, ch, getKeyChar(ch));
-		Bot.get().getKeyboard()._keyPressed(ke);
+		pressKey(ch, ch, 0, 0);
 	}
-	
-	public static void sendKey(final char ch, final int delay) {
-		boolean shift = false;
+
+	public static void releaseKey(final char ch, final int code, final int delay, final int mask) {
+		final KeyEvent e = getKeyEvent(KeyEvent.KEY_RELEASED, delay, mask, code, ch);
+		getKeyboard()._keyReleased(e);
+	}
+
+	/**
+	 * Used for releasing action keys. Eg. left, right, up, down, home
+	 *
+	 * @param code Key code from {@link KeyEvent}.
+	 * @param delay The time until the key is held down.
+	 */
+	public static void releaseKey(final int code, final int delay) {
+		releaseKey(KeyEvent.CHAR_UNDEFINED, code, delay, 0);
+	}
+
+	public static void releaseKey(final int code) {
+		releaseKey(KeyEvent.CHAR_UNDEFINED, code, 0, 0);
+	}
+
+	/**
+	 * Used for pressing "typeable" keys. Eg. 'a', 'b', 'c', '6'.
+	 *
+	 * @param ch The char to press.
+	 * @param delay The time until the key is held down.
+	 * @param mask The mask to press this key with.
+	 */
+	public static void releaseKey(final char ch, final int delay, final int mask) {
+		releaseKey(ch, ch, delay, mask);
+	}
+
+	public static void releaseKey(final char ch, final int delay) {
+		releaseKey(ch, ch, delay, 0);
+	}
+
+	public static void releaseKey(final char ch) {
+		releaseKey(ch, ch, 0, 0);
+	}
+
+	/**
+	 * @param type
+	 * @param delay
+	 * @param mask
+	 * @param code
+	 * @param ch
+	 * @return The {@link KeyEvent} associated with the given information.
+	 */
+	private static KeyEvent getKeyEvent(final int type, final int delay, final int mask, final int code, final char ch) {
+		return new KeyEvent(Bot.get().getClient(), type, System.currentTimeMillis() + delay, mask, code, ch, getLocation(code, ch));
+	}
+
+	/**
+	 * Returns the standard location of the character on the keyboard for masks.
+	 *
+	 * @param ch The character to determine the position of.
+	 * @return The key location value.
+	 */
+	private static int getLocation(final int code, final char ch) {
+		if (KeyEvent.VK_SHIFT <= code  && code <= KeyEvent.VK_ALT) {
+			return Random.nextInt(KeyEvent.KEY_LOCATION_LEFT, KeyEvent.KEY_LOCATION_RIGHT + 1);
+		}
+		return KeyEvent.KEY_LOCATION_STANDARD;
+	}
+
+	/**
+	 * Types a single character.
+	 *
+	 * @param ch The key to type.
+	 */
+	public static void sendKey(final char ch) {
+		sendKey(ch, 0);
+	}
+
+	/**
+	 * Presses and holds the given character for the delay, then releases.
+	 *
+	 * @param ch    The character to type.
+	 * @param delay The time to hold the key for.
+	 */
+	public static void sendKey(char ch, final int delay) {
 		int code = ch;
-		if ((ch >= 'a') && (ch <= 'z')) {
+		if (ch >= 'a' && ch <= 'z') {
 			code -= 32;
-		} else if ((ch >= 'A') && (ch <= 'Z')) {
-			shift = true;
 		}
-		KeyEvent ke;
-		if ((code == KeyEvent.VK_LEFT) || (code == KeyEvent.VK_UP) || (code == KeyEvent.VK_DOWN)) {
-			ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_PRESSED, System.currentTimeMillis() + delay, 0, code, getKeyChar(ch), KeyEvent.KEY_LOCATION_STANDARD);
-			Bot.get().getKeyboard()._keyPressed(ke);
-			final int delay2 = Random.nextInt(50, 120) + Random.nextInt(0, 100);
-			ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_RELEASED, System.currentTimeMillis() + delay2, 0, code, getKeyChar(ch), KeyEvent.KEY_LOCATION_STANDARD);
-			Bot.get().getKeyboard()._keyReleased(ke);
-		} else {
-			if (!shift) {
-				ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_PRESSED, System.currentTimeMillis() + delay, 0, code, getKeyChar(ch), KeyEvent.KEY_LOCATION_STANDARD);
-				Bot.get().getKeyboard()._keyPressed(ke);
-				// Event Typed
-				ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_TYPED, System.currentTimeMillis() + 0, 0, 0, ch, 0);
-				Bot.get().getKeyboard()._keyTyped(ke);
-				// Event Released
-				final int delay2 = Random.nextInt(50, 120) + Random.nextInt(0, 100);
-				ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_RELEASED, System.currentTimeMillis() + delay2, 0, code, getKeyChar(ch), KeyEvent.KEY_LOCATION_STANDARD);
-				Bot.get().getKeyboard()._keyReleased(ke);
-			} else {
-				// Event Pressed for shift key
-				final int s1 = Random.nextInt(25, 60) + Random.nextInt(0, 50);
-				ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_PRESSED, System.currentTimeMillis() + s1, InputEvent.SHIFT_DOWN_MASK, KeyEvent.VK_SHIFT, (char) KeyEvent.VK_UNDEFINED, KeyEvent.KEY_LOCATION_LEFT);
-				Bot.get().getKeyboard()._keyPressed(ke);
-
-				// Event Pressed for char to send
-				ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_PRESSED, System.currentTimeMillis() + delay, InputEvent.SHIFT_DOWN_MASK, code, getKeyChar(ch), KeyEvent.KEY_LOCATION_STANDARD);
-				Bot.get().getKeyboard()._keyPressed(ke);
-				// Event Typed for char to send
-				ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_TYPED, System.currentTimeMillis() + 0, InputEvent.SHIFT_DOWN_MASK, 0, ch, 0);
-				Bot.get().getKeyboard()._keyTyped(ke);
-				// Event Released for char to send
-				final int delay2 = Random.nextInt(50, 120) + Random.nextInt(0, 100);
-				ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_RELEASED, System.currentTimeMillis() + delay2, InputEvent.SHIFT_DOWN_MASK, code, getKeyChar(ch), KeyEvent.KEY_LOCATION_STANDARD);
-				Bot.get().getKeyboard()._keyReleased(ke);
-
-				// Event Released for shift key
-				final int s2 = Random.nextInt(25, 60) + Random.nextInt(0, 50);
-				ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_RELEASED, System.currentTimeMillis() + s2, InputEvent.SHIFT_DOWN_MASK, KeyEvent.VK_SHIFT, (char) KeyEvent.VK_UNDEFINED, KeyEvent.KEY_LOCATION_LEFT);
-				Bot.get().getKeyboard()._keyReleased(ke);
-			}
-		}
+		sendKey(ch, code, delay);
 	}
-	private static void sendKeys(final String text, final boolean pressEnter, final int minDelay, final int maxDelay) {
-		final char[] chs = text.toCharArray();
-		for (final char element : chs) {
-			sendKey(element, Random.nextInt(minDelay, maxDelay));
-			sleep(Random.nextInt(minDelay, maxDelay));
+
+	/**
+	 * Presses and holds the given character for the delay, then releases.
+	 *
+	 * @param ch    The character to type.
+	 * @param code  Key code for special characters
+	 * @param delay The time to hold the key for.
+	 */
+	public static void sendKey(char ch, int code, final int delay) {
+		final boolean shift = ch >= 'A' && ch <= 'Z' ? true : false;
+		pressKey(ch, code, delay, shift ? InputEvent.SHIFT_DOWN_MASK: 0);
+		sleep(50, 100);
+		releaseKey(ch, code, delay, shift ? InputEvent.SHIFT_DOWN_MASK : 0);
+	}
+
+	/**
+	 * Types an array of characters.
+	 *
+	 * @param text       The text to send.
+	 * @param pressEnter <tt>true</tt> to press enter; otherwise <tt>false</tt>.
+	 */
+	public static void sendText(final String text, final boolean pressEnter) {
+		sendText(text, pressEnter, 100, 200);
+	}
+
+	/**
+	 * Types an array of characters with the given delay between keys.
+	 *
+	 * @param text       The text to enter into the game.
+	 * @param pressEnter <tt>true</tt> to press enter; otherwise <tt>false</tt>.
+	 * @param minDelay   The lowest possible wait (inclusive).
+	 * @param maxDelay   The largest possible wait (exclusive).
+	 */
+	public static void sendText(final String text, final boolean pressEnter, final int minDelay, final int maxDelay) {
+		final char[] chars = text.toCharArray();
+		for (final char element : chars) {
+			final int wait = Random.nextInt(minDelay, maxDelay);
+			sendKey(element, wait);
+			if (wait > 0) {
+				sleep(wait);
+			}
 		}
 		if (pressEnter) {
 			sendKey((char) KeyEvent.VK_ENTER, Random.nextInt(minDelay, maxDelay));
 		}
 	}
-	
-	public static void releaseKey(char ch) {
-		KeyEvent ke;
-		ke = new KeyEvent(Bot.get().getClient(), KeyEvent.KEY_RELEASED, System.currentTimeMillis(), InputEvent.ALT_DOWN_MASK, ch, getKeyChar(ch));
-		Bot.get().getKeyboard()._keyReleased(ke);
-	}
-	
-	private static char getKeyChar(final char c){
-		if ((c >= 36) && (c <= 40))
-			return KeyEvent.VK_UNDEFINED;
-		else
-			return c;
+
+	/**
+	 * Returns the keyboard associated with this thread-group to relay events to.
+	 *
+	 * @return The <code>org.powerbot.game.client.input.Keyboard</code> to relay events to.
+	 */
+	private static nl.wbot.bot.input.Keyboard getKeyboard() {
+		return Bot.get().getKeyboard();
 	}
 }
